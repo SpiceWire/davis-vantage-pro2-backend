af356715220f50856da42c1d958fd1514de44ca1
SpiceWire <115056325+SpiceWire@users.noreply.github.com>
2023-08-10 02:33:38 -0400

    breakpoint for the night

--- src/main/java/spicewire/davisinterface/View/ViewDao.java
+++ src/main/java/spicewire/davisinterface/View/ViewDao.java
@@ -1,10 +1,13 @@
 package spicewire.davisinterface.View;
 
+import spicewire.davisinterface.Model.CurrentWeather;
+
 /**
  * Minimal methods needed by the view to interact with the controller.
  */
 public interface ViewDao {
     int getComPortIndex();
+    int getComPort();
     int getBaudRate();
     int getParity();
     int getStopBits();
@@ -12,10 +15,10 @@ public interface ViewDao {
     int getTimeoutMode();
     void setConsoleFriendlyTextArea(String friendlyText);
     void setCurrentDataTextArea();
-    void parseDiagnosticsReport(String diagnosticReport)
+    void parseDiagnosticsReport(String diagnosticReport);
     int getWriteTimeout();
     int getReadTimeout();
     void setTestDescriptionTextArea(String description);
     void setConsoleRawTextArea(String rawData);
-
+   //CurrentWeather getCurrentWeather();
 }
