78af8533b54e21d29d543d73e9e184799bbf4eea
SpiceWire <115056325+SpiceWire@users.noreply.github.com>
2023-09-08 21:39:49 -0400

    Both Rest controllers hav open CrossOrigin.

--- src/main/java/spicewire/davisinterface/Controller/WeatherRestController.java
+++ src/main/java/spicewire/davisinterface/Controller/WeatherRestController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import spicewire.davisinterface.Model.CurrentWeather;
 
-@CrossOrigin(origins = {"http://localhost:5173","http://localhost:8080"})
+@CrossOrigin
 @RestController
 @RequestMapping("/weather")
 public class WeatherRestController {
