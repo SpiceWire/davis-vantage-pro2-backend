6cd437d0960831adf69cbd1e758aa4ac9eb7d0ef
SpiceWire <115056325+SpiceWire@users.noreply.github.com>
2023-10-12 19:59:05 -0400

    added websocket controller

--- src/main/java/spicewire/davisinterface/Controller/WebsocketController.java
+++ src/main/java/spicewire/davisinterface/Controller/WebsocketController.java
@@ -1,5 +1,18 @@
 package spicewire.davisinterface.Controller;
 
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import spicewire.davisinterface.Dao.JdbcWeatherRecord;
+import spicewire.davisinterface.Model.CurrentWeather;
+
+@Controller
 public class WebsocketController {
 
+    private static JdbcWeatherRecord jdbcWeatherRecord;
+
+    @SendTo("/weather/mostRecent")
+    public CurrentWeather mostRecentWeather() {
+        return jdbcWeatherRecord.getWeather();
+    }
 }
