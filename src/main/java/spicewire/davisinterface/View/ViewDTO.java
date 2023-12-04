2e30e3c9cb4ccaec5708423cddb5a2267429b506
SpiceWire <115056325+SpiceWire@users.noreply.github.com>
2023-12-04 00:04:13 -0500

    Changed ViewDTO from static to non-static

--- src/main/java/spicewire/davisinterface/View/ViewDTO.java
+++ src/main/java/spicewire/davisinterface/View/ViewDTO.java
@@ -11,82 +11,82 @@ public class ViewDTO {
      * Holds binary results from Davis Loop and LPS commands.
      * Davis Command category: Current Data
      */
-    private static String currentDataText;
+    private  String currentDataText;
 
     /**
      * Holds Controller's response to Loop and LPS commands.
      * Davis Command category: Current Data
      */
-    private static String currentDataEvalText;
+    private  String currentDataEvalText;
 
     /**
      * Holds raw Davis Console results from testing commands: Test, RxCheck, RxTest, Ver, Nver, Receivers
      * Davis Command category: Testing
      */
-    private static String testingRawText;
+    private  String testingRawText;
 
     /**
      * Holds Controller-interpreted Davis Console results from testing commands: Test,
      * RxCheck, RxTest, Ver, Nver, Receivers
      * Davis Command category: Testing
      */
-    private static String testingFriendlyText;
+    private  String testingFriendlyText;
 
     /**
      * Holds description of the current Davis Testing command.
      * Davis Command category: Testing
      */
-    private static String testingDescription;
+    private  String testingDescription;
 
-    private static String lastCommandSent;
+    private  String lastCommandSent;
 
-    private static String errorMessage;
+    private  String errorMessage;
 
-    public static String getErrorMessage() {
+    public  String getErrorMessage() {
         return errorMessage;
     }
 
-    public static void setErrorMessage(String errorMessage) {
-        ViewDTO.errorMessage = errorMessage;
+    public  void setErrorMessage(String errorMessage) {
+        this.errorMessage = errorMessage;
     }
 
-    public static void setErrorMessage(int errorMessageNumber) {
-        ViewDTO.errorMessage = getErrorMessage(errorMessageNumber);
+    public  void setErrorMessage(int errorMessageNumber) {
+        this.errorMessage = getErrorMessage(errorMessageNumber);
     }
-    public static String getLastCommandSent() {
+    public  String getLastCommandSent() {
         return lastCommandSent;
     }
 
-    public static final String WRONG_TYPE = "The Command sent was the wrong type for the operation.";
-    public static final String UNKNOWN_TYPE = "The Command sent did not use a valid word, was unknown, " +
+    public  final String WRONG_TYPE = "The Command sent was the wrong type for the operation.";
+    public  final String UNKNOWN_TYPE = "The Command sent did not use a valid word, was unknown, " +
             "or not yet implemented";
-    public static final String WRONG_PARAMS = "The Command sent had missing or invalid parameters.";
-    public static final String NO_CONNECTION = "The console did not have a working connection to the backend.";
-    public static final String UNKNOWN = "An unexpected error occurred.";
-    public static final String CRC_ERROR = "An unresolvable CRC error occurred.";
+    public  final String WRONG_PARAMS = "The Command sent had missing or invalid parameters.";
+    public  final String NO_CONNECTION = "The console did not have a working connection to the backend.";
+    public  final String UNKNOWN = "An unexpected error occurred.";
+    public  final String CRC_ERROR = "An unresolvable CRC error occurred.";
 
-    public static void setLastCommandSent(String lastCommandSent) {
-        ViewDTO.lastCommandSent = lastCommandSent;
+    public  void setLastCommandSent(String lastCommandSent) {
+        this.lastCommandSent = lastCommandSent;
     }
 
-    public static void setCurrentDataText(String currentDataText) {
-        ViewDTO.currentDataText = currentDataText;
+    public  void setCurrentDataText(String currentDataText) {
+        this.currentDataText = currentDataText;
     }
 
-    public static String getCurrentDataText() {
+    public  String getCurrentDataText() {
         return currentDataText;
     }
 
-    public static String getCurrentDataEvalText() {
+    public  String getCurrentDataEvalText() {
         return currentDataEvalText;
     }
 
-    public static void setCurrentDataEvalText(String currentDataEvalText) {
-        ViewDTO.currentDataEvalText = currentDataEvalText;
+    public  void setCurrentDataEvalText(String currentDataEvalText) {
+        this.currentDataEvalText = currentDataEvalText;
     }
 
     /**
-     * Uses an index number to include an error message to the ViewDTO.
+     * Uses an index number to include an error message to the this.
      * Index is:       0 WRONG_TYPE,
      *                 1 UNKNOWN_TYPE,
      *                 2 WRONG_PARAMS,
@@ -95,7 +95,7 @@ public class ViewDTO {
      * @param errorNumber
      * @return
      */
-    public static String getErrorMessage(int errorNumber){
+    public  String getErrorMessage(int errorNumber){
         String[] errorMsgAry = new String[] {
                 WRONG_TYPE,
                 UNKNOWN_TYPE,
@@ -106,27 +106,27 @@ public class ViewDTO {
         return errorMsgAry[errorNumber];
     }
 
-    public static String getTestingRawText() {
+    public  String getTestingRawText() {
         return testingRawText;
     }
 
-    public static void setTestingRawText(String testingRawText) {
-        ViewDTO.testingRawText = testingRawText;
+    public  void setTestingRawText(String testingRawText) {
+        this.testingRawText = testingRawText;
     }
 
-    public static String getTestingFriendlyText() {
+    public  String getTestingFriendlyText() {
         return testingFriendlyText;
     }
 
-    public static void setTestingFriendlyText(String testingFriendlyText) {
-        ViewDTO.testingFriendlyText = testingFriendlyText;
+    public  void setTestingFriendlyText(String testingFriendlyText) {
+        this.testingFriendlyText = testingFriendlyText;
     }
 
-    public static String getTestingDescription() {
+    public  String getTestingDescription() {
         return testingDescription;
     }
 
-    public static void setTestingDescription(String testingDescription) {
-        ViewDTO.testingDescription = testingDescription;
+    public  void setTestingDescription(String testingDescription) {
+        this.testingDescription = testingDescription;
     }
 }
