fd5450b19754957e1ca7c7c57471c2aec6db1f1e
SpiceWire <115056325+SpiceWire@users.noreply.github.com>
2024-02-01 21:09:15 -0500

    Added JavaDoc comments

--- src/main/java/spicewire/davisinterface/Controller/RecordRestController.java
+++ src/main/java/spicewire/davisinterface/Controller/RecordRestController.java
@@ -7,6 +7,11 @@ import spicewire.davisinterface.Model.AggregateWeather;
 import java.time.LocalDateTime;
 import java.util.HashMap;
 
+/**
+ * This class maps frontend requests for weather records. It can handle
+ * requests for a specific day's weather,
+ */
+
 @CrossOrigin
 @RestController
 @RequestMapping("/record")
@@ -33,6 +38,13 @@ public class RecordRestController {
 //        return consoleController.getPreviousWeatherbyDay(offset);
 //    }
 
+    /**
+     * Returns an AggregateWeather object for a day offset from now().
+     * AggregateWeather object contains a field for cumulative rain since the
+     * offset day.
+     * @param offset number of days offset from now()
+     * @return AggregateWeather object serialized
+     */
     @GetMapping(value= "/rain/{val}")
     private AggregateWeather previousRain(@PathVariable Integer offset){
         System.out.println("Console received request for past weather.");
@@ -40,6 +52,12 @@ public class RecordRestController {
 
     }
 
+    /**
+     * Returns a hashmap paired values: (now() -i hours, header value at now() -i hours)
+     * for the past 24 hours, given a valid header.
+     * @param headerName valid name for header
+     * @return serialized hashmap of past 24 hours and header value at each hour
+     */
     @GetMapping(value= "/header/{val}")
     private HashMap<LocalDateTime, String> headerData24Hours(@PathVariable String headerName){
         System.out.println("Console received request for 24 hours of hourly data in a category " +
